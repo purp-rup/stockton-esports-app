6a769dc579b8d5447b5f713fc86a08acf3d165fd
purp-rup <miragliaandrew@gmail.com>
2025-09-27 14:46:56 -0400

    Imported base Flask project
    The base Flask project includes:
    - A package style directory for modular design
    - exampleModule.py that demonstrates this feature
    - static and templates directories
    - pyproject.toml file for build

---
+++ EsportsManagementTool/EsportsManagementTool/__init__.py
@@ -0,0 +1,15 @@
+# This Flask project is set up in the packages format, meaning we can
+# separate our application into multiple modules that are then imported
+# into __init__.py here
+
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+# Module imports
+import EsportsManagementTool.exampleModule
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+

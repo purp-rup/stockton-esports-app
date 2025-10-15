414e1c5cbe8b5394e8eb5ec02b5e83dd179be592
JCamp74 <jbcampbell10@yahoo.com>
2025-10-15 15:33:26 -0400

    Updated the event registry to work.
    Event registry now works! yay!

--- EsportsManagementTool/EsportsManagementTool/__init__.py
+++ EsportsManagementTool/EsportsManagementTool/__init__.py
@@ -213,15 +213,21 @@ def eventRegister():
         #Receives a user response for all of eventName, eventDate, eventTime, and eventDescription
         eventName = request.form.get('eventName', '').strip()
         eventDate = request.form.get('eventDate', '').strip()
-        eventTime = request.form.get('eventTime', '').strip()
+        eventType = request.form.get('eventType', '').strip()
+        game = request.form.get('game', '').strip()
+        startTime = request.form.get('startTime', '').strip()
+        endTime = request.form.get('endTime', '').strip()
         eventDescription = request.form.get('eventDescription', '').strip()
+        location = request.form.get('eventLocation', '').strip()
+
 
     #Does what needs to be done if the fields are filled out.
-        if eventName and eventDate and eventTime and eventDescription:
+        if eventName and eventDate and eventType and game and startTime and endTime and eventDescription:
             cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
             try:
-                cursor.execute('INSERT INTO events (name, date, time, description) VALUES (%s, %s, %s, %s)', (eventName, eventDate, eventTime, eventDescription))
-
+                cursor.execute('INSERT INTO generalevents (EventName, Date, StartTime, EndTime, Description, EventType, Game, Location) '
+                                'VALUES (%s, %s, %s, %s, %s, %s, %s, %s)',
+                                (eventName, eventDate, startTime, endTime, eventDescription, eventType, game, location))
                 #Confirms that the event is registered.
                 mysql.connection.commit()
                 msg = 'Event Registered!'
